284674417050c868daf6a88398199377df4c7e18
malcomowilla <owillamalcom@gmail.com>
2023-11-20 15:35:36 +0300

    deploying on render8

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,8 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://captive-portal.onrender.com 
-    "
+    origins "https://captive-portal.onrender.com" 
+    
 
     resource '/stk_push',
       headers: :any,
